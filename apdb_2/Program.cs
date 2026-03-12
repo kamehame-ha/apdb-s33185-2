55b472944a0147459cb3de783481d6f11403dd48
kamehame_ha <bartek@kluz.pro>
2026-03-12 12:42:36 +0100

    Added new feature

--- apdb_2/Program.cs
+++ apdb_2/Program.cs
@@ -2,6 +2,11 @@
 {
     public static int Add(int a, int b) => a + b;
     public static int Sub(int a, int b) => a - b;
+
+    public int AverageValues(int[] values)
+    {
+        return values.Sum(); 
+    }
     public static void Main(string[] args)
     {
         Console.WriteLine(Add(1,2));
