068d7346f2f093ad4befa694fff8d823941f1ba6
kamehame_ha <bartek@kluz.pro>
2026-03-12 12:44:56 +0100

    Fixed new method

--- apdb_2/Program.cs
+++ apdb_2/Program.cs
@@ -3,9 +3,9 @@
     public static int Add(int a, int b) => a + b;
     public static int Sub(int a, int b) => a - b;
 
-    public int AverageValues(int[] values)
+    public double AverageValues(int[] values)
     {
-        return values.Sum(); 
+        return values.Average();
     }
     public static void Main(string[] args)
     {
