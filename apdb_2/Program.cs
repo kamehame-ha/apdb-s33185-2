18dabd6c416864ec75302d135bae9434f00ee6bb
kamehame_ha <bartek@kluz.pro>
2026-03-12 12:39:31 +0100

    Added tests for 2 methods

--- apdb_2/Program.cs
+++ apdb_2/Program.cs
@@ -4,6 +4,7 @@
     public static int Sub(int a, int b) => a - b;
     public static void Main(string[] args)
     {
-
+        Console.WriteLine(Add(1,2));
+        Console.WriteLine(Sub(1,2));
     }
 }
